3248c598ba988b53b756ed132f22a71ba5652aaf
Fabi <mima7@users.noreply.github.com>
2018-10-26 15:48:27 +0300

    upload

---
+++ Training_Fabian_2018/SIF_example/sif_interface.sv
@@ -0,0 +1,27 @@
+interface sif_i(input bit clk);
+    logic rst_n;
+    logic xa_wr_s, xa_rd_s, wa_wr_s;
+    logic [15:0] xa_addr, wa_addr, xa_data_rd, xa_data_wr, wa_data_wr;
+
+    modport TB (
+        input clk,
+        input xa_data_rd, wa_addr, wa_data_wr,
+        output rst_n,
+        output xa_addr, xa_data_wr,
+        output xa_wr_s, xa_rd_s, wa_wr_s//enables
+    );
+
+    modport DUT (
+        input clk, rst_n,
+        input xa_addr, xa_data_wr,
+        input xa_wr_s, xa_rd_s, wa_wr_s//enables
+        output xa_data_rd, wa_addr, wa_data_wr
+    );
+
+    modport MONITOR(
+        input clk, rst_n,
+        input xa_addr, xa_data_wr, xa_data_rd,
+        input wa_addr, wa_data_wr,
+        input xa_wr_s, xa_rd_s, wa_wr_s//enables
+    );
+endinterface //sif_i
---
+++ Training_Fabian_2018/SIF_example/sif_tb.sv
@@ -0,0 +1,20 @@
+module sif_tb;
+
+sif_i tb_ports;
+/*
+modport TB (
+        input clk,
+        input xa_data_rd, wa_addr, wa_data_wr,
+        output rst_n,
+        output xa_addr, xa_data_wr,
+        output xa_wr_s, xa_rd_s, wa_wr_s//enables
+    );
+*/
+
+
+
+
+initial begin
+    
+end
+endmodule
