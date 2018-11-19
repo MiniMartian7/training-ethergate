f221b5da3b64af539996182cedce67c5cca8944d
Fabian <fabian@eth.server>
2018-11-19 13:48:44 +0200

    just a test

--- Training_Fabian_2018/SIF_example/library_pack.sv
+++ Training_Fabian_2018/SIF_example/library_pack.sv
@@ -1,3 +1,4 @@
 package library_pack;
     typedef enum logic [2:0] {WRITE = 3'b110, READ = 3'b101, IDLE = 3'b100, ILLEGAL = 3'b111, RESET = 3'b000} E_Operation;
 endpackage: library_pack
+/*aceasta este modificare ce trebuie sa apara in github*/
